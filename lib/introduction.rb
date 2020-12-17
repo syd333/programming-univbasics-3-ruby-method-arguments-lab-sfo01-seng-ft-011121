f1cfcdb73dbe43fea5809a97bd344f54063e874a
sydney miranda <sydmiranda@icloud.com>
2020-12-17 15:45:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,17 +10,17 @@ def introduction name
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language name, language
+def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional name, language="Ruby"
+def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction
-introduction_with_language
-introduction_with_language_optional
+introduction ("Sydney")
+introduction_with_language ("Sydney", "React")
+introduction_with_language_optional ("Cyntelle")
 
 
 
